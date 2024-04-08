492d887e0ee5f8c53191d2e3899f737e96ac8e97
sujin27 <leesu7855@dgu.ac.kr>
2024-04-08 14:20:15 +0900

    maxmin code M

--- Caps/minmax.py
+++ Caps/minmax.py
@@ -4,6 +4,13 @@ from sklearn.preprocessing import MinMaxScaler
 # 데이터 불러오기
 data = pd.read_excel("E:/LiFePO4 Dynamic Profile Files/LiFePO4 Dynamic Profile Files/10도/LiFePO4_DST_SOC_10_1.xlsx")
 
+# 비수치 열 제외
+numerical_columns = ['Test_Time(s)', 'Step_Time(s)', 'Step_Index', 'Current(A)', 'Voltage(V)', 'Temperature (C)_1', 'SOC(t)']
+data_numeric = data[numerical_columns]
+
+# 결측치 처리
+data_numeric = data_numeric.dropna()
+
 # 정규화할 열 선택
 columns_to_normalize = ['Current(A)', 'Voltage(V)', 'Temperature (C)_1', 'SOC(t)']
 
@@ -11,7 +18,7 @@ columns_to_normalize = ['Current(A)', 'Voltage(V)', 'Temperature (C)_1', 'SOC(t)
 scaler = MinMaxScaler()
 
 # 선택한 열에 대해 Min-Max 스케일링 적용
-data[columns_to_normalize] = scaler.fit_transform(data[columns_to_normalize])
+data_normalized = pd.DataFrame(scaler.fit_transform(data_numeric[columns_to_normalize]), columns=columns_to_normalize)
 
 # 정규화된 데이터 출력
-print(data.head())
+print(data_normalized.head())
